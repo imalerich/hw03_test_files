6b784b0b4a1b3e26986542c8d606810b76868978
Ian Malerich <ian.malerich@gmail.com>
2015-10-22 00:58:54 +0000

    more testing

--- test.sh
+++ test.sh
@@ -9,5 +9,8 @@ echo ../analyze df=data/data1.csv if=input/in0.txt of=output/out1.txt
 echo ../analyze df=data/data1.csv if=input/in1.txt of=output/out2.txt
 ../analyze df=data/data1.csv if=input/in1.txt of=output/out2.txt
 
+echo ../analyze df=data/data1.csv if=input/in2.txt of=output/out3.txt
+../analyze df=data/data1.csv if=input/in2.txt of=output/out3.txt
+
 echo ../analyze df=data/AmesWeatherData.csv if=input/ames0.txt of=output/ames0.txt
 ../analyze df=data/AmesWeatherData.csv if=input/ames0.txt of=output/ames0.txt
